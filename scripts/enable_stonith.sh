216d40034a912251603e84b814393543a0ac892b
Remus Buzatu <r.buzatu@gmail.com>
2018-08-28 00:27:06 +0200

    Added script for enabling fencing

--- scripts/enable_stonith.sh
+++ scripts/enable_stonith.sh
@@ -6,12 +6,12 @@ IMAGES_DIR=$BASE_DIR/../images/
 UNDERCLOUD_RC_FILE=$BASE_DIR/stackrc
 
 source $UNDERCLOUD_RC_FILE
-for i in `ironic node-list | grep -i control | awk '{print $2}'`
-    do ironic node-show $i | grep -i ipmi_address
+user=root
+passwd=Changeme_123
+for i in `openstack baremetal node list -c Name -f value | grep -i control`; do
+  ip=`openstack baremetal node show $i -c driver_info -f json | jq .driver_info.ipmi_address | tr -d '"'`;echo "$i - $ip"
+  echo pcs stonith create ipmi-$i fence_ipmilan pcmk_host_list=$i ipaddr=$ip login=$user passwd=$passwd lanplus=1; echo pcs constraint location ipmi-$i avoids $i
+  echo '##############'
 done
-
-pcs stonith create ipmi-controller-1 fence_ipmilan pcmk_host_list=controller-1 ipaddr=$IP_CONTOLLER_1 login=root passwd=calvin lanplus=1; pcs constraint location ipmi-controller-1 avoids controller-1
-pcs stonith create ipmi-controller-2 fence_ipmilan pcmk_host_list=controller-2 ipaddr=$IP_CONTOLLER_2 login=root passwd=calvin lanplus=1; pcs constraint location ipmi-controller-2 avoids controller-2
-pcs stonith create ipmi-controller-3 fence_ipmilan pcmk_host_list=controller-3 ipaddr=$IP_CONTOLLER_3 login=root passwd=calvin lanplus=1; pcs constraint location ipmi-controller-3 avoids controller-3
 
 pcs property set stonith-enabled=true
