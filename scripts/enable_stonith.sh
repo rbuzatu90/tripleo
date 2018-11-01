af8b11ef82946ec6a9edca39013948192c0dda90
Remus Buzatu <r.buzatu90@gmail.com>
2018-11-01 04:17:49 +0400

    Added power_timout for fencing agent

--- scripts/enable_stonith.sh
+++ scripts/enable_stonith.sh
@@ -10,7 +10,7 @@ user=root
 passwd=Changeme_123
 for i in `openstack baremetal node list -c Name -f value | grep -i control`; do
   ip=`openstack baremetal node show $i -c driver_info -f json | jq .driver_info.ipmi_address | tr -d '"'`;echo "$i - $ip"
-  echo pcs stonith create ipmi-$i fence_ipmilan pcmk_host_list=$i ipaddr=$ip login=$user passwd=$passwd lanplus=1; echo pcs constraint location ipmi-$i avoids $i
+  echo pcs stonith create ipmi-$i fence_ipmilan pcmk_host_list=$i ipaddr=$ip login=$user passwd=$passwd lanplus=1 retry_on=3 power_timeout=180; echo pcs constraint location ipmi-$i avoids $i
   echo '##############'
 done
 
