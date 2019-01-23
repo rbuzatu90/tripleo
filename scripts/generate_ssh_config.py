eb783d74a4afcd1349e4ea0eafb968443c6ca2c5
Remus Buzatu <r.buzatu90@gmail.com>
2019-01-23 11:26:45 +0400

    Generate ansible hosts file
    Signed-off-by: Remus Buzatu <r.buzatu90@gmail.com>

--- scripts/generate_ssh_config.py
+++ scripts/generate_ssh_config.py
@@ -41,6 +41,36 @@ def gen_ssh_config():
       x=line.split("|")
       print "Host", x[6]
 
-gen_director_ssh_config()
+def gen_ansible_config():
+  controller = []
+  compute = []
+  compute_dpdk = []
+  compute_sriov = []
+  contrail = []
+
+  for line in data:
+    info = line.split("|")
+    #print info
+    if "cntrl" in info[2]:
+      controller.append(info[6].split("=")[1])
+    if "comp" in info[2]:
+      compute.append(info[6].split("=")[1])
+    if "dsdn" in info[2]:
+      contrail.append(info[6].split("=")[1])
+  print '[controller]'
+  for i in controller:
+    print i
+  print '[compute]'
+  for i in compute:
+    print i
+  print '[contrail]'
+  for i in contrail:
+    print i
+  print '[undercloud]'
+  print subnet + '.1'
+  
+       
+#gen_director_ssh_config()
 #gen_etc_hosts()
 #gen_ssh_config()
+gen_ansible_config()
