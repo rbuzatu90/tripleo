4bbd9c5f46e15a2a9e7e325d9784149f9cca274d
Remus Buzatu <r.buzatu90@gmail.com>
2019-05-01 01:58:41 +0300

    We don't need 749 port

--- scripts/setup_idm.sh
+++ scripts/setup_idm.sh
@@ -57,7 +57,7 @@ DOMAIN=mylab.test
 for i in _ldap._tcp _kerberos._tcp _kerberos._udp _kerberos-master._tcp _kerberos-master._udp _ntp._udp ; do dig @${NAMESERVER} ${i}.${DOMAIN} srv +nocmd +noquestion +nocomments +nostats +noaa +noadditional +noauthority; done | egrep "^_"
 
 # Check client has open ports for IdM integration
-ports="80 443 88 389 636 464 749"
+ports="80 443 88 389 636 464"
 hosts="10.237.191.78 10.237.191.79 10.237.221.78 10.237.221.79"
 for port in $ports; do
     for host in $hosts; do
