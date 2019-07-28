6616454a1f5c55c59f315ade0b78487710d3b230
Remus Buzatu <r.buzatu90@gmail.com>
2019-07-28 15:43:48 +0400

    Added ssh key to ironic inspector PXE

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -1,6 +1,7 @@
 for i in `ironic node-list | grep power | awk '{print $2}'`; do ironic node-delete $i;done
 for i in `nova list | grep -v 'Status\|\+' | awk '{print $2}'`; do nova delete $i;done
 for i in `nova list | grep ACTIVE | awk '{print $12}' | grep -o [0-9.]*`;do ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null heat-admin@$i ls;done
+for i in `nova list | grep ACTIVE | awk '{print $12}' | grep -o [0-9.]*`;do rsync --rsync-path="sudo rsync" ../.ssh/id_rsa $i:/root/.ssh/;done
 for i in `ironic node-list | grep "None\|True" | awk '{print $2}'`; do ironic node-set-power-state $i on ; ironic node-set-maintenance $i off ;done
 nova list | grep Running | awk '{print $4, $12}' | sed 's/ctlplane=//g' | awk -F '-' '{print $7}'
 
