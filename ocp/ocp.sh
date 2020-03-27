34255dfb83a9670738358cb579bc7e336f22e31b
Remus Buzatu <r.buzatu90@gmail.com>
2020-03-27 11:32:33 -0400

    Whole init for OCP

--- ocp/ocp.sh
+++ ocp/ocp.sh
@@ -1,20 +1,49 @@
-openstack role add --user admin --project admin swiftoperator
-openstack object store account set --property Temp-URL-Key=superkey
+
 openstack project create openshift
 openstack role add --user admin --project openshift admin
 openstack quota set --cores -1 --gigabytes -1 --floating-ips -1 --ram -1 openshift
 
-fip1=`openstack floating ip create --floating-ip-address 192.168.122.167 public-net -c floating_ip_address -f value`
+openstack role add --user admin --project admin swiftoperator
+openstack object store account set --property Temp-URL-Key=superkey
+
+glance image-create --name "rhel7" --visibility public --disk-format qcow2 --container-format bare --progress --file $IMAGES_DIR/rhel-server-7.7-update-1-x86_64-kvm.qcow2
+openstack image create --container-format=bare --disk-format=qcow2 --file rhcos-4.3.0-x86_64-openstack.qcow2 rhcos 
+
+
+for i in `openstack security group list -c ID -f csv | grep -v ID | tr -d '"'`; do
+    openstack security group rule create --protocol icmp $i
+    openstack security group rule create  --dst-port 1:65000 $i
+done
+
+openstack keypair create --public-key ~/.ssh/id_rsa.pub mykey
+
+openstack flavor create m1.small --id auto --ram 1024 --disk 30 --vcpus 2
+
+neutron net-create private-net --provider:network_type vxlan
+subnet="10.0.0"
+neutron subnet-create private-net --dns-nameserver 8.8.8.8 --enable-dhcp --allocation-pool start=$subnet.60,end=$subnet.100 --gateway $subnet.1 $subnet.0/24 --name private-subnet
+
+neutron net-create --router:external --provider:network_type flat --provider:physical_network datacentre public-net
+subnet="192.168.122"
+neutron subnet-create public-net --dns-nameserver $subnet.5 --enable-dhcp --allocation-pool start=$subnet.160,end=$subnet.200 --gateway $subnet.1 $subnet.0/24 --name public-subnet
+
+fip1=`openstack floating ip create --floating-ip-address 192.168.122.167 public-net -c floating_ip_address -f value` # FIP for ocp admin VM
+openstack floating ip create --floating-ip-address 192.168.122.166 public-net # api
+openstack floating ip create --floating-ip-address 192.168.122.177 public-net # *.apps
+
+neutron router-create external-router
+neutron router-interface-add external-router private-subnet
+neutron router-gateway-set external-router public-net
+
+
 secgrp=`openstack security group list --project admin -c ID -f value`
 netid=`openstack network list --internal  | grep private | awk '{print $2}'`
-vmid1=`openstack server create --image rhel7.7 --flavor m1.small --security-group $secgrp --key-name mykey --nic net-id=$netid ocp -c id -f value`
+vmid1=`openstack server create --image rhel7 --flavor m1.small --security-group $secgrp --key-name mykey --nic net-id=$netid ocp -c id -f value`
+sleep 100
 openstack server add floating ip $vmid1 $fip1
 
 #neutron subnet-update --dns-nameserver 172.24.16.111 --dns-nameserver 172.24.16.112 `openstack subnet list | grep ocp | grep node | awk '{print $2}'`
-openstack image create --container-format=bare --disk-format=qcow2 --file rhcos-4.3.0-x86_64-openstack.qcow2 rhcos 
 
-openstack floating ip create --floating-ip-address 192.168.122.166 public-net # api
-openstack floating ip create --floating-ip-address 192.168.122.177 public-net # *.apps
 
 openstack flavor create --ram 8192 --disk 50 --vcpu 8 --private --project openshift  --insecure  ocp.master.big
 openstack flavor create --ram 8192 --disk 50 --vcpu 8 --private --project openshift  --insecure  ocp.infra.big
