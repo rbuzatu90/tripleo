5b8fe7dab73c3e20a6225579f85ea3436f8bd7ff
Remus Buzatu <r.buzatu90@gmail.com>
2020-03-27 08:12:37 -0400

    Fixed issues in init_overcloud

--- scripts/init_overcloud.sh
+++ scripts/init_overcloud.sh
@@ -22,18 +22,19 @@ echo "Stack complete is $STACK_COMPLETE and deployment exited with $DEPLOYMENT_R
 if [[ $DEPLOYMENT_RESULT -eq 0 ]] && [[ $STACK_COMPLETE -eq 0 ]]; then
     source $OVERCLOUD_RC_FILE
     glance image-create --name "centos" --visibility public --disk-format qcow2 --container-format bare --progress --file $IMAGES_DIR/CentOS-7-x86_64-GenericCloud.qcow2
+    glance image-create --name "rhel7" --visibility public --disk-format qcow2 --container-format bare --progress --file $IMAGES_DIR/rhel-server-7.7-update-1-x86_64-kvm.qcow2
     # set password for centos image; add public key, install tcpdump nmap wget
     # lower DHCP timeout on image virt-customize -a CentOS-7-x86_64-GenericCloud.qcow2 --run-command 'echo -e "timeout 20;\nretry 10;" > /etc/dhcp/dhclient.conf'
     #glance image-create --name "cirros" --is-public true --disk-format qcow2 --container-format bare --progress --file $IMAGES_DIR/cirros-0.3.5-x86_64-disk.img
-
-    for i in `openstack security group list -c ID -f csv | grep -v ID`; do
+    
+    for i in `openstack security group list -c ID -f csv | grep -v ID | tr -d '"'`; do
         openstack security group rule create --protocol icmp $i
         openstack security group rule create  --dst-port 1:65000 $i
     done
 
     openstack keypair create --public-key ~/.ssh/id_rsa.pub mykey
 
-    openstack flavor create m1.small --id 1 --ram 512 --disk 9 --vcpus 1
+    openstack flavor create m1.small --id auto --ram 1024 --disk 30 --vcpus 2
 
     neutron net-create private-net --provider:network_type vxlan
     subnet="10.0.0"
@@ -46,10 +47,10 @@ if [[ $DEPLOYMENT_RESULT -eq 0 ]] && [[ $STACK_COMPLETE -eq 0 ]]; then
     neutron router-create external-router
     neutron router-interface-add external-router private-subnet
     neutron router-gateway-set external-router public-net
-
-    netid=`openstack network list --internal -c ID -f value`
-    vmid1=`openstack server create --image centos --flavor m1.small --security-group default --key-name mykey --nic net-id=$netid vm1 -c id -f value`
-    vmid2=`openstack server create --image centos --flavor m1.small --security-group default --key-name mykey --nic net-id=$netid vm2 -c id -f value`
+    secgrp=`openstack security group list --project admin -c ID -f value`
+    netid=`openstack network list --internal  | grep private | awk '{print $2}'`
+    vmid1=`openstack server create --image centos --flavor m1.small --security-group $secgrp --key-name mykey --nic net-id=$netid vm1 -c id -f value`
+    vmid2=`openstack server create --image centos --flavor m1.small --security-group $secgrp --key-name mykey --nic net-id=$netid vm2 -c id -f value`
     pubnetid=`openstack network list --external -c ID -f value`
     fip1=`openstack floating ip create $pubnetid -c floating_ip_address -f value`
     fip2=`openstack floating ip create $pubnetid -c floating_ip_address -f value`
